c93e77c7b11b898a0f9d09c556ef31931072c847
Aramex86 <slavbez@mail.ru>
2021-05-16 17:46:01 +0300

    add datetext input in WeekPiker

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -1,27 +1,25 @@
 export const addDays = (date: any, daysToAdd: any) => {
-    const clone = new Date(date.getTime())
-    clone.setDate(clone.getDate()+daysToAdd)
-    console.log(clone);
-    return clone
+  const clone = new Date(date.getTime());
+  clone.setDate(clone.getDate() + daysToAdd);
+  return clone;
 };
 
-export const getWeek=(forDate:any, daysOffset = 0)=> {
-    const date = addDays(forDate, daysOffset); 
-    const day = date.getDay(); 
-    return {
+export const getWeek = (forDate: any, daysOffset = 0) => {
+  const date = addDays(forDate, daysOffset);
+  const day = date.getDay();
+  return {
     date,
-    start: addDays(date, -day), 
-    end: addDays(date, 6 - day) 
-    };
-   }
+    start: addDays(date, -day),
+    end: addDays(date, 6 - day),
+  };
+};
 
+export const getData = (url: any) => {
+  return fetch(url).then((res) => {
+    if (!res.ok) {
+      throw Error("There was a problem fetching data.");
+    }
 
- export const getData=(url:any)=>  {
-     return fetch(url).then(res=> {
-         if(!res.ok){
-             throw Error("There was a problem fetching data.")
-         }
-
-         return res.json()
-     })
- }
+    return res.json();
+  });
+};
--- src/components/Bookings/WeekPiker.tsx
+++ src/components/Bookings/WeekPiker.tsx
@@ -1,36 +1,58 @@
-import {useReducer} from "react";
+import React, { useReducer, useRef, useState } from "react";
 import reducer from "./reducer";
-import {getWeek} from "../../Utils/utils";
-import {FaChevronLeft, FaCalendarDay, FaChevronRight} from "react-icons/fa";
+import { getWeek } from "../../Utils/utils";
+import {
+  FaChevronLeft,
+  FaCalendarDay,
+  FaChevronRight,
+  FaCalendarCheck,
+} from "react-icons/fa";
 
- const WeekPicker= ({date}:any)=> {
+const WeekPicker = ({ date }: any) => {
   const [week, dispatch] = useReducer(reducer, date, getWeek);
+  const [dateText, setDateText] = useState("2020-06-24");
+
+  // const textboxRef = useRef<any>();
+
+  function goToDate() {
+    dispatch({
+      type: "SET_DATE",
+      // payload: textboxRef.current.value,
+      payload: dateText,
+    });
+  }
 
   return (
     <div>
       <p className="date-picker">
-        <button
-          className="btn"
-          onClick={() => dispatch({type: "PREV_WEEK"})}
-        >
-          <FaChevronLeft/>
+        <button className="btn" onClick={() => dispatch({ type: "PREV_WEEK" })}>
+          <FaChevronLeft />
           <span>Prev</span>
         </button>
 
-        <button
-          className="btn"
-          onClick={() => dispatch({type: "TODAY"})}
-        >
-          <FaCalendarDay/>
+        <button className="btn" onClick={() => dispatch({ type: "TODAY" })}>
+          <FaCalendarDay />
           <span>Today</span>
         </button>
 
-        <button
-          className="btn"
-          onClick={() => dispatch({type: "NEXT_WEEK"})}
-        >
+        <span>
+          <input
+            type="text"
+            // ref={textboxRef}
+            placeholder="e.g. 2020-09-02"
+            value={dateText}
+            onChange={(e) => setDateText(e.target.value)}
+          />
+
+          <button className="go btn" onClick={goToDate}>
+            <FaCalendarCheck />
+            <span>Go</span>
+          </button>
+        </span>
+
+        <button className="btn" onClick={() => dispatch({ type: "NEXT_WEEK" })}>
           <span>Next</span>
-          <FaChevronRight/>
+          <FaChevronRight />
         </button>
       </p>
       <p>
@@ -38,6 +60,6 @@ import {FaChevronLeft, FaCalendarDay, FaChevronRight} from "react-icons/fa";
       </p>
     </div>
   );
-}
+};
 
-export default WeekPicker
+export default WeekPicker;
